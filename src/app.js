2626407c9593e07b2fa5197db8037c9c8de69c91
danielsumah <danielsumah@gmail.com>
2022-08-28 17:09:08 +0100

    express routes config, dependency config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
+const routes= require('../src/frameworks/express/routes');
+
+const API_URL = process.env.API_URL || '/api/v1';
+
+const dependencies= require('./config/dependencies');
 
 module.exports = {
     start: () => {
+        //Middlewares
+        app.use(express.json());
+        app.use(express.urlencoded({extended:true}));
+
+
+        //Routes
+        app.use(API_URL, routes(dependencies))
+
+
+        //Common error handlers
+
         app.listen(PORT, ()=>{
             console.log("Server running on port", PORT);
         })
